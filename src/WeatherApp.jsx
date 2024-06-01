8ce6e57184890a1e662b45a7615abc87a1a8eb2c
WGreenwichChaca <walter.greenwich@gmail.com>
2024-06-01 01:36:21 -0500

    Añadiendo diseño responsivo

--- src/WeatherApp.jsx
+++ src/WeatherApp.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import WeatherIcon from "./components/WeatherIcon";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 export const WeatherApp = () => {
   const urlBase = "https://api.openweathermap.org/data/2.5/weather";
@@ -17,10 +19,9 @@ export const WeatherApp = () => {
     e.preventDefault();
     if (ciudad.length > 0) {
       await fetchClima();
-      setCiudad(""); 
+      setCiudad("");
     }
   };
-  
 
   const fetchClima = async () => {
     try {
@@ -33,19 +34,29 @@ export const WeatherApp = () => {
   };
 
   return (
-    <div className="container">
+    <div className="min-h-screen  bg-customGray text-white p-8 w-full sm:w-11/12 md:w-4/5 lg:w-2/5 my-16 mx-auto rounded-lg">
+      <div className="flex flex-col md:flex-row justify-between items-center">
+        <form onSubmit={handleSubmit} className="relative w-full md:w-auto mb-4 md:mb-0">
+          <input 
+            type="text" 
+            placeholder="Buscar por ciudad" 
+            className="w-full md:w-auto pl-10 pr-5 py-2 bg-customGray border-none outline-none text-white rounded" 
+            value={ciudad} 
+            onChange={handleCambioCiudad} />
+          <FontAwesomeIcon icon={faMagnifyingGlass} className="absolute top-1/2 left-3 transform -translate-y-1/2 text-gray-300" />
+        </form>
+        <div className="text-lg sm:text-xl lg:text-2xl flex items-center">
+          <span className="mr-2">°C</span>
+          <span >°F</span>
+        </div>
+      </div>
       <h1>Aplicación del Clima</h1>
-      <form onSubmit={handleSubmit}>
-        <input type="text" value={ciudad} onChange={handleCambioCiudad} />
-        <button type="submit">Buscar</button>
-      </form>
       {dataClima && (
         <div>
           <h2>{dataClima.name}</h2>
           <p>Temperatura: {Math.round(dataClima.main.temp - difKelvin)}°C</p>
           <p>Condición metereológica: {dataClima.weather[0].description}</p>
           <WeatherIcon iconCode={dataClima?.weather[0]?.icon} />
-
         </div>
       )}
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customGray: '#171717',
+      }
+    },
   },
   plugins: [],
 }
